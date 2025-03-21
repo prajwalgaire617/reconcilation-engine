a07783bb1f176958b5dbbe5ffc9aa5ff0ad774c5
sniedzielski <sniedzielski@soldevelo.com>
2025-03-21 14:49:30 +0100

    OP-2335: added logger to change language in order to track issue

--- core/schema.py
+++ core/schema.py
@@ -1658,11 +1658,17 @@ def set_user_deleted(user):
 
 
 def change_user_language(user, language_id):
-    updated_user = User.objects.get(id=user.id)
-    updated_user.i_user.language_id = language_id
-    updated_user.save()
-    cache_key = InteractiveUserSerializer().get_cache_key(updated_user.i_user)
-    cache.delete(cache_key)
+    try:
+        updated_user = User.objects.get(id=user.id)
+        updated_user.i_user.language_id = language_id
+        updated_user.save()
+        cache_key = InteractiveUserSerializer().get_cache_key(updated_user.i_user)
+        logger.debug("[OpenIMISMutation] Deleting cache for user with cache key: %s", cache_key)
+        cache.delete(cache_key)
+    except Exception as exc:
+        logger.error("[OpenIMISMutation] Failed to change user language for user %s: %s", user.id, str(exc))
+        raise
+
 
 class ChangePasswordMutation(graphene.relay.ClientIDMutation):
     """
