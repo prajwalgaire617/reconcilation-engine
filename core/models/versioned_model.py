d88f5df27bb31616cebe0a9a6ad7fed6396ec7bb
Maxime NGOE <maxime.ngoe@gmail.com>
2025-03-28 12:12:34 +0100

    Cache management on versioned_model.py (#365)
    * Cache management on versioned_model.py
    
    This PR enhances the caching logic for unique object retrieval in the custom manager used within openIMIS. The changes ensure that unique lookups (by id, pk, or uuid) are consistently normalized and cached regardless of whether they are provided as keyword arguments. This results in cache key generation, improved cache hit rates, and reduced unnecessary database queries.
    
    * Update versioned_model.py
    
    Add Save and Delete process
    
    * added super at the top of the save
    
    * OP-2350: fixed handling kwargs on save method
    
    * OP-2350: removed redundant print
    
    ---------
    
    Co-authored-by: Patrick Delcroix <patrick.delcroix@swisstph.ch>
    Co-authored-by: sniedzielski <sniedzielski@soldevelo.com>

--- core/models/versioned_model.py
+++ core/models/versioned_model.py
@@ -1,8 +1,8 @@
 import uuid
 from copy import copy
 from datetime import datetime as py_datetime
+from django.core.cache import caches
 from django.db import models
-from django.core.cache import cache
 
 from core.utils import (
     clear_cache,
@@ -13,11 +13,94 @@ from ..utils import filter_validity
 import logging
 
 logger = logging.getLogger(__name__)
+CACHE_TIMEOUT = 3600 * 24
+cache = caches["default"]
+
+
+class CachedManager(models.Manager):
+    
+    def get(self, *args, **kwargs):
+        """
+        Overrides the get() method to check Redis cache before
+        performing a DB lookup for simple unique lookups.
+        """
+        unique_fields = ('pk', 'id', 'uuid')
+        cache_key = None
+
+        # Case 1: Simple kwargs lookup.
+        if kwargs and len(kwargs) == 1:
+            key = list(kwargs.keys())[0]
+            if key in unique_fields:
+                value = kwargs[key]
+                # Convert UUID objects to string for the cache key.
+                if key in ('id', 'pk'):
+                    try:
+                        # Convert to int if possible.
+                        value = int(value)
+                    except (ValueError, TypeError):
+                        pass
+                if isinstance(value, uuid.UUID):
+                    value = str(value)
+                cache_key = get_cache_key(self.__class__, value)
+        # use case for Family Request elements in args
+        elif not kwargs and args and len(args) == 1 :
+            if len(args[0].children) == 1:
+                field, value = args[0].children[0]
+                if field in unique_fields:
+                    cache_key = get_cache_key(self.__class__, value)
+
+        # If we constructed a cache key, try to retrieve from the cache.
+        if cache_key:
+            cached_instance = cache.get(cache_key)
+            if cached_instance is not None:
+                logger.debug("Returning cached instance for key: %s", cache_key)
+                return cached_instance
+
+            # Not in cache; perform DB lookup.
+        instance = super().get(*args, **kwargs)
+        cache.set(cache_key, instance, timeout=None)
+        logger.debug("Cached instance %s after DB lookup", cache_key)
+        return instance
 
 
 class BaseVersionedModel(models.Model):
     validity_from = DateTimeField(db_column='ValidityFrom', default=py_datetime.now)
     validity_to = DateTimeField(db_column='ValidityTo', blank=True, null=True)
+
+    # Use our custom CachedManager for object retrieval
+    objects = CachedManager()
+
+    def update(self, *args, **kwargs):
+        """
+        Overrides the default update to update the cache after saving the instance.
+        """
+        super().update(*args, **kwargs)
+        cache.set(get_cache_key(self.__class__, self.id), self,  timeout=CACHE_TIMEOUT)
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the default save to update the cache after saving the instance.
+        """
+        caching = kwargs.pop('cache_update', True)
+        super().save(*args, **kwargs)
+        if caching:
+            # Build the cache key using the same logic as in the CachedManager.
+            # (Assuming lookups are done using pk/id/uuid)
+            cache.set(get_cache_key(self.__class__, self.id), self,  timeout=CACHE_TIMEOUT)
+            logger.debug("Saved and cached instance: %s", self)
+        else:
+            clear_cache(self)
+        return self
+
+    def delete(self, *args, **kwargs):
+        """
+        Overrides the default delete to remove the instance from the cache.
+        """
+        # Build the cache key prior to deletion.
+        clear_cache(self)
+        logger.debug("Clear cached instance: %s", self)
+        # Then perform the actual deletion.
+        return super().delete(*args, **kwargs)
 
     def save_history(self, **kwargs):
         if not self.id:  # only copy if the data is being updated
@@ -28,7 +111,7 @@ class BaseVersionedModel(models.Model):
             setattr(histo, "uuid", uuid.uuid4())
         histo.validity_to = py_datetime.now()
         histo.legacy_id = self.id
-        histo.save()
+        histo.save(cache_update=False)
         return histo.id
 
     def delete_history(self, **kwargs):
@@ -49,44 +132,13 @@ class BaseVersionedModel(models.Model):
         return queryset
 
 
-class CachedManager(models.Manager):
-    def get(self, *args, **kwargs):
-        pk = None
-        if 'pk' in kwargs:
-            pk = kwargs['pk']
-        if 'id' in kwargs:
-            pk = kwargs['id']
-        if pk:
-            cache_key = get_cache_key(self.model, pk)
-            cached_instance = cache.get(cache_key)
-            if cached_instance is not None:
-                return cached_instance
-
-        instance = super().get(*args, **kwargs)
-        if instance.pk:
-            cache_key = get_cache_key(self.model, instance.pk)
-            cache.set(cache_key, instance, timeout=18000)
-
-        return instance
 
 
 class VersionedModel(BaseVersionedModel):
     legacy_id = models.IntegerField(
         db_column='LegacyID', blank=True, null=True)
 
-    objects = CachedManager()
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        cache.set(get_cache_key(self.__class__, self.id), self,  timeout=18000)
-
-    def update(self, *args, **kwargs):
-        super().update(*args, **kwargs)
-        cache.set(get_cache_key(self.__class__, self.id), self,  timeout=18000)
-
-    def delete(self, *args, **kwargs):
-        super().delete(*args, **kwargs)
-        clear_cache(self)
 
     class Meta:
         abstract = True
