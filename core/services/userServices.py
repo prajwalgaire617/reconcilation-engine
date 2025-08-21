fb623cdfbbabb569c8b6089e23c0c7983a2f10ff
kevel choumene <k.choumene@cloudfacile.com>
2025-08-21 19:48:54 +0100

    fix: fix validity_to on core_user

--- core/services/userServices.py
+++ core/services/userServices.py
@@ -216,6 +216,12 @@ def create_or_update_core_user(user_uuid, username, i_user=None, t_user=None, of
         user = None
         created = False
 
+    if user and user.i_user and i_user:
+        if user.i_user != i_user:
+            from core import datetime
+            now = datetime.datetime.now()
+            user.validity_from = now
+            user.validity_to = None
     if not user:
         user = User(username=username)
         created = True
